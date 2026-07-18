a8661d447cb1f5a73ad308fbc80b51531854bc62
Basit Ghani <basitghani021@gmail.com>
2026-07-18 16:22:08 +0500

    \Fix white screen: Hardened vercel.json and production build"

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2092997895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2325701335" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
